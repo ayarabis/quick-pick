a201679e2a5ba2c19661b64d0d55140c1f33c956
Aldrin Arabis <arabis.aldrin@yandex.com>
2023-04-03 14:07:50 +0800

    chore: add release action

---
+++ release.config.js
@@ -0,0 +1,11 @@
+const config = {
+	branches: ['main'],
+	plugins: [
+		'@semantic-release/commit-analyzer',
+		'@semantic-release/release-notes-generator',
+		'@semantic-release/npm',
+		'@semantic-release/git'
+	]
+};
+
+export default config;
